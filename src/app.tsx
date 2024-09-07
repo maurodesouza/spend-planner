b0f24f937f3583394f880f871f0bcfd95ff20cb2
Mauro de Souza <maurodesouza2017@hotmail.com>
2024-09-07 19:13:41 -0300

    ✨  feat: add initial interface

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,22 @@
+import { Input } from "./components/ui/input";
+import { Button } from "./components/ui/button";
+import { CurrencyInput } from "./components/ui/currency-input";
+
 export function App() {
   return (
-    <h1>Ahhh... Hello!??</h1>
+    <div className="h-full p-4">
+      <div className="flex items-center justify-between gap-4">
+        <form className="flex items-center gap-2">
+          <Input type="color" />
+          <Input />
+          <CurrencyInput onChange={console.log} />
+
+          <Button type="submit">Add</Button>
+        </form>
+        <div>
+          <CurrencyInput onChange={console.log} />
+        </div>
+      </div>
+    </div>
   )
 }
